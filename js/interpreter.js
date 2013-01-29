838b004c0fbdd56d0d90967fcd7643d1b09a9d27
bernien2s <b.sexton@warwick.ac.uk>
2013-01-29 01:42:24 +0000

    SVG Drawing Engine

--- js/interpreter.js
+++ js/interpreter.js
@@ -6,6 +6,12 @@ function loadNextEdenCode(options) {
 	options.editor.setValue(eden.nextHistory());
 }
 
+function insertTag(options, value) {
+    var code = document.getElementById("txtarea").value; 
+    $("#txtarea").focus();
+	options.editor.setValue(code + value);
+}
+
 function submitEdenCode(options) {
 	var editor = options.editor;
 	var edenparser = options.edenparser;
@@ -83,6 +89,42 @@ function make_interpreter(name, mtitle, edenparser) {
 
 			buttons: [
 				{
+					id: "btn-in",
+					text: "Styles",
+					click: function() {
+						$( "#dialog" ).dialog();
+						$("a").click(function () {
+        					var selectbox = document.getElementsByName($(this).attr("id"));
+							
+							try {
+								var item = selectbox[0].options[selectbox[0].selectedIndex].value;
+        					} catch (err) {
+        						console.log("you cannot do this...");
+        					}
+        					
+        					if(item!=undefined) {
+        						insertTag({editor: myeditor}, item);
+        					}
+    					});
+						
+						$('#svgcanvas').attr("currentScale", function() { return parseFloat(this.currentScale) + 0.50; });
+						root.lookup('zoomLevel').assign($('#svgcanvas').attr("currentScale"));
+					}
+				},
+				{
+					id: "btn-out",
+					text: "Export",
+					click: function() {
+						var svgfile = document.getElementById("svgcanvas");
+						w = window.open('','Model State','width=400,height=400,scrollbars=no,toolbar=no,directories=no,status=no,resizable=yes');
+						b = w.document.getElementsByTagName('body')[0];
+						b.appendChild(svgfile);
+						
+						$('#svgcanvas').attr("currentScale", function() { return parseFloat(this.currentScale) - 0.50; });
+						root.lookup('zoomLevel').assign($('#svgcanvas').attr("currentScale"));
+					}
+				},
+				{
 					id: "btn-submit",
 					text: "Submit",
 					click: function() {
@@ -105,6 +147,8 @@ function make_interpreter(name, mtitle, edenparser) {
 		});
 	input_dialog = $dialog;
 
+	$("#btn-in").css("margin-left", "0px");
+	$("#btn-out").css("margin-left", "0px");
 	$("#btn-submit").css("margin-right", "30px");
 
 	myeditor = convertToEdenPageNew('#'+name+'-input','code');
